5207f8edad31cac14f742785849f58c49f3a6152
Ichii <45918948+IchiiDev@users.noreply.github.com>
2021-01-23 19:38:44 +0100

    Added request processing

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const process = require("./package.json");
 const express = require("express");
 const bodyparser = require("body-parser");
 const { checkWebHook, postWebHook } = require("./utils/webhooks");
+const { renderDate } = require("./utils/date");
 
 console.log(`Starting "\x1b[33m${process.name}\x1b[0m" app.\n\x1b[32mVersion:\x1b[0m ${process.version}\n\x1b[32mAuthor:\x1b[0m ${process.author}`);
 
@@ -21,16 +22,21 @@ let router = express.Router();
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
-router.post("webhook", (req, res) => {
-    if (req.headers.authorization != config.AUTH) return res.json({ message: "403 Forbidden"}, 403);
-    if (req.body.bot == undefined || config.BOTS[req.body.bot] == undefined) return res.json({ message: "400 Bad Request" }, 400);
-    if (req.body.user == undefined) return res.json({ message: "400 Bad Request" }, 400);
-    if (req.body.type == undefined) return res.json({ message: "400 Bad Request" }, 400);
-    if (req.body.isWeekend == undefined) return res.json({ message: "400 Bad Request" }, 400);
+router.post("/webhook", (req, res) => {
+    if (req.headers.authorization != config.AUTH) return res.status(403).json({ message: "403 Forbidden" });
+    if (req.body.bot == undefined || config.BOTS[req.body.bot] == undefined) return res.status(400).json({ message: "400 Bad Request" });
+    if (req.body.user == undefined) return res.status(400).json({ message: "400 Bad Request" });
+    if (req.body.type == undefined) return res.status(400).json({ message: "400 Bad Request" });
+    if (req.body.isWeekend == undefined) return res.status(400).json({ message: "400 Bad Request" });
+    if (req.body.isWeekend == "true") req.body.isWeekend = true;
+    if (req.body.isWeekend == "false") req.body.isWeekend = false;
 
-    let message = ``
+    let bot = config.BOTS[req.body.bot];
+
+    let message = `\`[${renderDate()}]\` The user \`${req.body.user}\` has voted for the bot **${bot}**${(req.body.query != undefined) ? ` (Request Query: \`${req.body.query}\`)` : ""}. ${(req.body.isWeekend == true) ? `**This vote count as double since the weekend boost is applied.**` : ""}`;
     postWebHook(config.DISCORD_WEBHOOK, message)
         .catch(error => { throw error; });
+    res.status(200).json({ message: "All clear !"});
 });
 
 app.use(router);
