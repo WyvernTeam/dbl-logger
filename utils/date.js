09ed90b9dbd816d54fd30fefda79b1eb1353b395
Ichii <45918948+IchiiDev@users.noreply.github.com>
2021-01-23 19:38:22 +0100

    Added utils

---
+++ utils/date.js
@@ -0,0 +1,11 @@
+function renderDate(d = new Date()) {
+    const years = `${d.getFullYear().toString()}`;
+    const month = `${(d.getMonth() + 1).toString().length === 2 ? (d.getMonth() + 1).toString() : `0${(d.getMonth() + 1).toString()}`}`;
+    const day = `${d.getDate().toString().length === 2 ? d.getDate().toString() : `0${d.getDate().toString()}`}`;
+    const hours = `${d.getHours().toString().length === 2 ? d.getHours().toString() : `0${d.getHours().toString()}`}`;
+    const minutes = `${(parseInt(d.getMinutes() / 5) * 5).toString().length === 2 ? (parseInt(d.getMinutes() / 5) * 5).toString() : `0${(parseInt(d.getMinutes() / 5) * 5).toString()}`}`;
+    const seconds = `${d.getSeconds().toString().length === 2 ? d.getSeconds().toString() : `0${d.getSeconds().toString()}`}`;
+    return `${day}-${month}-${years} ${hours}:${minutes}:${seconds}`;
+}
+
+module.exports = { renderDate };
