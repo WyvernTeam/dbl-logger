aaa2934ee66edc91d0a60fdfa40fb5bbb5d419da
Ethan Thierry <45918948+IchiiDev@users.noreply.github.com>
2021-02-23 12:36:00 +0100

    Add comments in config.exemple.js

--- config.exemple.js
+++ config.exemple.js
@@ -1,8 +1,9 @@
 module.exports = {
-    PORT: 3000,
-    AUTH: "",
-    DISCORD_WEBHOOK: "",
-    APP_TOKEN: "",
+    PORT: 3000, // Set this to the port you want to listen to with the API.
+    AUTH: "", // The secret to authenticate DBL requests. Use a random string generator
+    DISCORD_WEBHOOK: "", // The webhook to post log messages to
+    APP_TOKEN: "", // Discord Bot Token for user requests (/!\ Do not use your own token)
+    // Set every bot here with this values: "Bot ID": "Bot name",
     BOTS: {
         "ID": ""
     }
