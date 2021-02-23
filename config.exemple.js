8c236699b24b33e9adec12bd2584e5b5d11c0f2e
Ethan Thierry <45918948+IchiiDev@users.noreply.github.com>
2021-02-23 11:32:21 +0100

    Add Utils

--- config.exemple.js
+++ config.exemple.js
@@ -2,6 +2,7 @@ module.exports = {
     PORT: 3000,
     AUTH: "",
     DISCORD_WEBHOOK: "",
+    APP_TOKEN: "",
     BOTS: {
         "ID": ""
     }
---
+++ utils/discord.js
@@ -0,0 +1,21 @@
+const { json } = require("body-parser");
+const fetch = require("node-fetch");
+const { APP_TOKEN } = require("../config.js");
+
+function getUserData(id) {
+    return new Promise((resolve, reject) => {
+        const data = {
+            method: "GET",
+            headers: { "Authorization": `Bot ${APP_TOKEN}` }
+        };
+        fetch(`https://discord.com/api/users/${id}`, data)
+            .then(response => response.json())
+            .then(json => {
+                if (json.message != undefined) reject(json.message());
+                resolve(json);
+            })
+            .catch(error => reject(error));
+    });
+}
+
+module.exports = { getUserData }
