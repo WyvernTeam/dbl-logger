6cecf60deb24a57ca5fb3599b568dbbc6ee3a7a1
Ethan Thierry <45918948+IchiiDev@users.noreply.github.com>
2021-02-23 11:37:35 +0100

    Add user resolution on log message

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const { checkWebHook, postWebHook } = require("./utils/webhooks");
 const { renderDate } = require("./utils/date");
+const { getUserData } = require("./utils/discord");
 
 console.log(`Starting "\x1b[33m${process.name}\x1b[0m" app.\n\x1b[32mVersion:\x1b[0m ${process.version}\n\x1b[32mAuthor:\x1b[0m ${process.author}`);
 
@@ -32,8 +33,12 @@ router.post("/webhook", (req, res) => {
     if (req.body.isWeekend == "false") req.body.isWeekend = false;
 
     let bot = config.BOTS[req.body.bot];
+    let user;
+    getUserData(req.body.user)
+        .then(result => { user = result})
+        .catch(error => { throw error; });
 
-    let message = `\`[${renderDate()}]\` The user \`${req.body.user}\` has voted for the bot **${bot}**${(req.body.query != "") ? ` (Request Query: \`${req.body.query}\`)` : ""}. ${(req.body.isWeekend == true) ? `**This vote count as double since the weekend boost is applied.**` : ""}`;
+    let message = `\`[${renderDate()}]\` The user ${user.username}#${user.discriminator} \`(${req.body.user})\` has voted for the bot **${bot}**${(req.body.query != "") ? ` (Request Query: \`${req.body.query}\`)` : ""}. ${(req.body.isWeekend == true) ? `**This vote count as double since the weekend boost is applied.**` : ""}`;
     postWebHook(config.DISCORD_WEBHOOK, message)
         .catch(error => { throw error; });
     res.status(200).json({ message: "All clear !"});
